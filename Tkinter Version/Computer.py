acdbc12c26677cef7e2875650f9b9f741c70f980
gpharr9 <72763907+gpharr9@users.noreply.github.com>
2020-12-03 22:01:32 -0600

    Create Computer.py

---
+++ Tkinter Version/Computer.py
@@ -0,0 +1,8 @@
+from Player import Player
+
+class Computer(Player):
+
+    # function to calculate the next best move...
+    # Currently not functioning
+    def step(self):
+        print('make calculation')

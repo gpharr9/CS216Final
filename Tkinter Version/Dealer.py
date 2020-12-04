5a0fc6cf8fb30f2413f389426319cb9a7126d4b9
gpharr9 <72763907+gpharr9@users.noreply.github.com>
2020-12-03 22:01:47 -0600

    Create Dealer.py

---
+++ Tkinter Version/Dealer.py
@@ -0,0 +1,19 @@
+from Hand import Hand
+from Player import Player
+
+class Dealer(Player):
+
+  def print_hand(self): # Output of the dealers current hand by using data made in card and hand
+    self.hand.print(True)
+
+
+  def hit(self, player): # Adds a card to the dealers deck based on whether the deck is below 17 or not
+    if(player):
+      player.hand.add_card()
+    else:
+      self.hand.add_card()
+
+  #Only used by player
+  # deprecated for now
+  # def split(self):
+  #   print('split')

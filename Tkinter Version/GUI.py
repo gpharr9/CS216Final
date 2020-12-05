2bbb59c80baee1469a9bd9e65ddd611d2988bc64
gpharr9 <72763907+gpharr9@users.noreply.github.com>
2020-12-05 14:39:26 -0600

    Update GUI.py

--- Tkinter Version/GUI.py
+++ Tkinter Version/GUI.py
@@ -1,63 +1,63 @@
 # import the modules || Source: https://www.geeksforgeeks.org/color-game-python/
 import tkinter
+from tkinter import *
 
-
-
-class GUI:
-
-    def set_up(self):
-        # create a GUI window 
-        self.root = tkinter.Tk() 
+def set_up():
+    # create a GUI window 
+    root = tkinter.Tk() 
   
-        # set the title 
-        self.root.title("Black Jack Game") 
+    # set the title 
+    root.title("Black Jack Game") 
   
-        # set the size 
-        self.root.geometry("500x500") 
+    # set the size 
+    root.geometry("500x500") 
   
-        # add instructions label 
-        instructions1 = tkinter.Label(self.root, text = "=====================================================================================================", 
-                                      font = ('Helvetica', 12)) 
-        instructions1.pack()  
+    # add instructions label 
+    instructions1 = tkinter.Label(root, text = "=====================================================================================================", 
+                                  font = ('Helvetica', 12)) 
+    instructions1.pack()  
 
-        instructions2 = tkinter.Label(self.root, text = "1) Blackjacks primary goal is to beat the dealers hand while keeping your hand under a total value of 21", 
-                                      font = ('Helvetica', 12)) 
-        instructions2.pack()  
+    instructions2 = tkinter.Label(root, text = "1) Blackjacks primary goal is to beat the dealers hand while keeping your hand under a total value of 21", 
+                                  font = ('Helvetica', 12)) 
+    instructions2.pack()  
 
-        instructions3 = tkinter.Label(self.root, text = "2) Choosing hit will have you draw another card and add it to your total hands value", 
-                                      font = ('Helvetica', 12)) 
-        instructions3.pack()
+    instructions3 = tkinter.Label(root, text = "2) Choosing hit will have you draw another card and add it to your total hands value", 
+                                  font = ('Helvetica', 12)) 
+    instructions3.pack()
+    
+    instructions4 = tkinter.Label(root, text = "3) Choosing stand will lock your hand in and compare it to the dealers", 
+                                  font = ('Helvetica', 12)) 
+    instructions4.pack()
 
-        instructions4 = tkinter.Label(self.root, text = "3) Choosing stand will lock your hand in and compare it to the dealers", 
-                                      font = ('Helvetica', 12)) 
-        instructions4.pack()
+    instructions5 = tkinter.Label(root, text = "=====================================================================================================", 
+                                  font = ('Helvetica', 12)) 
+    instructions5.pack()
 
-        instructions5 = tkinter.Label(self.root, text = "=====================================================================================================", 
-                                      font = ('Helvetica', 12)) 
-        instructions5.pack()
+    question = tkinter.Label(root, text = "Do you want to play a round (y/n)?")
 
-        question = tkinter.Label(self.root, text = "Do you want to play a round (y/n)?")
+    question.pack()
+    var1 = StringVar()
+    entry = tkinter.Entry(root, textvariable=var1)
 
-        question.pack()
-        entry = tkinter.Entry(self.root)
-        entry.bind("<Return>")
-        
-        entry.pack()
-        gui = GUI()
+    entry.pack()
+    def res():
+        result = var1.get()
+        if result in "Yy":
+            resultentry.insert(0, "True")
+        elif result in "Nn":
+            resultentry.insert(0, "False")
+    resultbutton = Button(root, text = "Enter", command=res)
+    resultbutton.pack()
 
-        self.state = entry.get()
-        self.state = gui.user_input(self.state)
-        # start the GUI
+    resultlabel = Label(root, text = "Test")
+    resultlabel.pack()
 
-        self.root.mainloop() 
+    resultentry = Entry(root)
+    resultentry.pack()
+    root.mainloop()
+    # start the GUI
 
-        if self.state == True or False:
-            return self.state
-        else:
-            self.root.mainloop()
 
-    def user_input(self, state):
-        if state in "Yy":
-            return True
-        elif state in "Nn":
-            return False
+
+
+set_up()

c71726eba8489c471db5265ad1cb8c845ebb1ac8
gpharr9 <72763907+gpharr9@users.noreply.github.com>
2020-12-09 19:19:39 -0600

    Delete Computer.py

--- Tkinter Version/Computer.py
+++
@@ -1,8 +0,0 @@
-from Player import Player
-
-class Computer(Player):
-
-    # function to calculate the next best move...
-    # Currently not functioning
-    def step(self):
-        print('make calculation')

2bd3d2af74a650aa461dd9247009ba4fff72eb25
gpharr9 <72763907+gpharr9@users.noreply.github.com>
2020-12-09 19:20:18 -0600

    Update Game.py

--- Tkinter Version/Game.py
+++ Tkinter Version/Game.py
@@ -11,52 +11,36 @@ class Game:
 
         self.dealer_instance = Dealer() # Dealer Object
         self.dealer_instance.create_hand() # Dealer Hand
-        # self.computer_instance = Computer()
+
+
+
         self.step = 0
 
 
-#    def next(self): # Primary game controller, allows everything to progress
-#        # first step, show deal and player hand
-#        player, dealer = self.print()
-#        # second step, prompt the user to what they want to do
-#        #self.ui = self.player_instance.collect_input()
+    def get_p_count(self):
+        p_count = self.player_instance.count_hand() # Checks total value of the Player hand
 
-#        if(d_state == 0 and p_state == 1): # Determines if the game is a tie of 21
-#            return 2
+        return p_count
 
-#        if d_state == 0 or d_state == 1: # Determines if the game has a winner or not
-#            return d_state
-#        if p_state == 0 or p_state == 1: # Determines if the game has a winner or not
-#            return p_state
-        
-        # system("cls")
 
-#        self.print() # Prints out hands
-        
-        
-    def get_count(self):
-        self.p_count = self.player_instance.count_hand() # Checks total value of the Player hand
-        
-        self.d_count = self.dealer_instance.count_hand() # Checks total value of the Dealer hand
+    def get_d_count(self):
+        d_count = self.dealer_instance.count_hand() # Checks total value of the Dealer hand
 
-        return self.p_count, self.d_count
-
+        return d_count
 
     def make_stand(self):
-        self.p_count = self.player_instance.count_hand() # Checks total value of the Player hand
-        self.d_count = self.dealer_instance.count_hand() # Checks total value of the Dealer hand
+        p_count = self.player_instance.count_hand() # Checks total value of the Player hand
+        d_count = self.dealer_instance.count_hand() # Checks total value of the Dealer hand
 
-        # print("Player Count: " + str(pCount))
-        # print("Dealer Count: " + str(dCount))
 
-        if(self.p_count > self.d_count): # Player wins
+        if(p_count > d_count): # Player wins
             return 0
-        elif(self.p_count < self.d_count): # Dealer wins
+        elif(p_count < d_count): # Dealer wins
             return 1
-        elif(self.p_count == self.d_count): # Tie game
+        elif(p_count == d_count): # Tie game
             return 2
-    
-    
+
+
     def condition(self):
         #print("Player Hand:") 
         p_arr = self.player_instance.print_hand() # Outputs the card layout based on player hand to the terminal
@@ -70,10 +54,10 @@ class Game:
 
     def p_hit(self):
         self.dealer_instance.hit(self.player_instance)
-        self.p_count = self.player_instance.count_hand()
-        if(self.p_count > 21): # Determines if the player has busted
+        p_count = self.player_instance.count_hand()
+        if(p_count > 21): # Determines if the player has busted
             return 1
-        elif(self.p_count == 21): # Determines if the player has won
+        elif(p_count == 21): # Determines if the player has won
             return 0
 
 
@@ -82,35 +66,15 @@ class Game:
         return state
 
 
-#    def validate_player(self): # Function to monitor player hand and ensure it follows game restrictions
-#        # check player count
-#        if(self.ui in 'Hh'): # Uses user input gathered in Player subclass to find if hitting or standing
-#            print('player hitting')
-#            self.dealer_instance.hit(self.player_instance) # Sends data to dealer in order to hit
-#            p_count = self.player_instance.count_hand() # Determines hands total value
-#            print("Player Count: " + str(p_count)) # Used for validating that code is working
-
-#          if(p_count > 21): # Determines if the player has busted
-#                return 1
-#            elif(p_count == 21): # Determines if the player has won
-#                return 0
-#        elif(self.ui in 'Ss'): # Prepares the players hand to end game
-#            print('player makin a stand')
-#            state = self.make_stand()
-#            return state
-
     def validate_dealer(self): # Function to monitor dealer hand and ensure if follows game restrictions
         # check dealer count
-        self.d_count = self.dealer_instance.count_hand() # Gathers dealers hand total value for the logic
-#        print("Dealer Count: " +str(d_count)) # Used for code validation
-        if(self.d_count > 21): # Determines if the dealer has busted
+        d_count = self.dealer_instance.count_hand() # Gathers dealers hand total value for the logic
+        if(d_count > 21): # Determines if the dealer has busted
             return 0
-        elif(self.d_count == 21): # Determines if the dealer has won
+        elif(d_count == 21): # Determines if the dealer has won
             return 1
-        if(self.d_count < 17): # Basic logic to determine dealer action
-            #print('dealer hitting')
+        if(d_count < 17): # Basic logic to determine dealer action
             self.dealer_instance.hit(None)
-        elif(self.d_count > 17): # Basic logic to determine dealer action
-            #print('dealer makin a stand')
+        elif(d_count > 17): # Basic logic to determine dealer action
             state = self.make_stand()
             return state
